ccc8cff13341ebb08ef08ab20afc9eb470dd5d02
najat <najatt.ismail@gmail.com>
2022-10-22 15:37:01 +0300

    filter funtionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,29 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/events-list';
+import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
-    return (
-      <EventList items={events} />
-    )
+  //we need a function to call the onSearch function/prop
+  //this function takes the same 2 parameters 
+  //and returns the path to be executed when the form is submitted 
+  //and we extracted our input/selected values
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    //go to a diffrent page
+    router.push(fullPath);
   }
-  export default AllEventsPage;
-  
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
